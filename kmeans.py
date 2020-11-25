572f25b275c0dd7559f41ab5de6e7a6008a9ad2c
leppekj <leppekj@uchicago.edu>
2020-11-24 19:42:57 -0600

    trying to figure out best number of clusters

--- kmeans.py
+++ kmeans.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import fen_vectors as fv
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
 
 def kmeans(data_csv, num_clusters):
     """
@@ -36,3 +38,27 @@ def get_indices(clustNum, labels_array):
     credit to https://stackoverflow.com/questions/36195457/python-sklearn-kmeans-how-to-get-the-samples-points-in-each-clusters
     """
     return np.where(labels_array == clustNum)[0]
+
+
+def plot_curve(k_options, data_csv):
+    """
+    tries multiple options of k (list)
+    https://stackoverflow.com/questions/41540751/sklearn-kmeans-equivalent-of-elbow-method
+    """
+    data = pd.read_csv(data_csv)
+    clusters = [KMeans(n_clusters=i).fit(data) for i in k_options]
+    # three methods
+    scores = [i.score(data) for i in clusters]
+    inertias = [i.inertia_ for i in clusters]
+    distances = [np.average(np.min(cdist(data, i.cluster_centers_, 'euclidean'), axis=1)) for i in clusters]
+    #plot
+    fig, axs = plt.subplots(3)
+    fig.suptitle('curves')
+    axs[0].plot(k_options, scores)
+    axs[0].set_title('sklean scores')
+    axs[1].plot(k_options, inertias)
+    axs[1].set_title('inertias?')
+    axs[2].plot(k_options, distances)
+    axs[2].set_title('distances from each cluster center')
+    plt.show()
+    return (k_options, scores, inertias, distances)
